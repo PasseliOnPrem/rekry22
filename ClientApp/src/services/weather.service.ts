0807a473580fdef0e2e9e20a4725c5249be5180e
WaldoPas <48987153+WaldoPas@users.noreply.github.com>
2021-12-20 13:04:08 +0200

    Fixed wrong import type

--- ClientApp/src/services/weather.service.ts
+++ ClientApp/src/services/weather.service.ts
@@ -1,11 +1,11 @@
-import { networkRequestError, weather } from '@/types'
+import { networkRequestError, weatherData } from '@/types'
 import axios, { AxiosResponse } from 'axios'
 
-export async function getWeather(): Promise<weather[] | networkRequestError> {
+export async function getWeather(): Promise<weatherData[] | networkRequestError> {
     //const url = import.meta.env.BASE_URL + "/weatherforecast"
     const url = "https://localhost:7033/weatherforecast"
     try {
-        const response = await axios.get<weather[]>(url)
+        const response = await axios.get<weatherData[]>(url)
         return response.data
     } catch (error) {
         return new networkRequestError(error)
